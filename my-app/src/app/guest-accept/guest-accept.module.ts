2d3c1597224cce6f97c33a682c061c01f786bec6
Moinam Chatterjee <moinamc2007@yahoo.co.in>
2020-11-04 16:24:07 +0530

    initial setup
    new Angular project

---
+++ my-app/src/app/guest-accept/guest-accept.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SpeakerListComponent } from './speaker-list/speaker-list.component';
+
+
+
+@NgModule({
+  declarations: [SpeakerListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GuestAcceptModule { }
